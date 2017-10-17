19063596868a380231f0ef54dcca92f592580f20
Michael Long <mikeylong04@googlemail.com>
2017-10-18 00:57:11 +0100

    Added my_all?, my_any? and my_none? methods to the enumerable module

--- advanced_building_blocks/enumerable.rb
+++ advanced_building_blocks/enumerable.rb
@@ -19,12 +19,36 @@ module Enumerable
 		self.my_each {|item| selected.push(item) if yield(item)}
 		selected
 	end
+	
+	def my_all?
+		result = true
+		self.my_each {|item| result = false  unless yield(item)}
+		result
+	end
+	
+	def my_any?
+		result = false
+		self.my_each {|item| result = true if yield(item)}
+		result
+	end
+	
+	def my_none?
+		result = true
+		self.my_each {|item| result = false if yield(item)}
+		result
+	end
+=begin	
+	def my_count(var=nil, &block=nil)
+		count = 0
+		self.my_each{|item| count += 1 if yield(item)}
+		count
+	end
+=end
 end
 
 my_array = [1,2,3,4,5,6,7,8,9,10]
-my_array.my_each {|num| print "#{num * 2}, "}
-puts
-my_array.my_each_with_index {|num, index| puts "#{index + 1}: #{num * 3}"}
-puts (my_array.my_select {|num| num > 8}).to_s
 
-puts (["hello", "goodbye"].my_select {|string| string[-1] == "e"}).to_s
+puts my_array.my_none? {|num| num < 0}
+puts my_array.my_none?{|num| num == 2}
+
+puts my_array.my_count{7}
